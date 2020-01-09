7f7b06f502612148838f8185a243132a56d9a204
pet100 <52423716+pet100@users.noreply.github.com>
2020-01-09 12:19:05 +0100

    Update 1233VPN-wg.sh

--- 1233VPN-wg.sh
+++ 1233VPN-wg.sh
@@ -4,9 +4,10 @@
 # Copyright (C) 2016-2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
 
 die() {
-    echo "[-] Error: Oops, your account number seems to be wron $1" >&2
-    exit 1
+        echo "[-] Error: Oops, your account number seems to be wron $1" >&2
 
+        exit 1
+}
 
 PROGRAM="${0##*/}"
 ARGS=( "$@" )
@@ -22,72 +23,71 @@ type curl >/dev/null || die "Please install curl and then try again."
 type jq >/dev/null || die "Please install jq and then try again."
 
 read -p "[?] Please enter your VPN account number: " -r APIKEY
+##################################################################################################################################
 
-function FR_VPN {
-        #echo "Lancement de la sauvegarde"
+    	function Server-FR {
+        echo "Contacting AzireVPN API in Toronto, Canada."
 }
 
-function PL_VPN {
-        #echo "Lancement de la restauration"
+function Server-PL {
+        echo "[*..] Contacting AzireVPN API in Paris, France."
 }
 
-function ALL_VPN {
-        
+function Server-ALL {
+        echo "[*..] Contacting AzireVPN API in Poland, Olsztyn."
 }
 
-echo "[?] Please choose on which server you want to add a configuration."
-PS3="Votre choix (1 - 2 - 3 or 4) : "
+PS3="Votre choix : "
 
-select item in "- Server-FR_PARIS -" "- Server-PL_WAW -" "- Server-ALL -" "- EXIT -"
+select item in "- Server-FR -" "- Server-PL -" "- Server-ALL -" "- Exit -"
 do
     for var in $REPLY; do
         echo "Vous avez choisi l'item $var : $item"
         case $var in
+##################################################################################################################################
+
                 1)
                         # Appel de la fonction sauve
-                        FR_VPN
+                        Server-FR
+                        RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"11","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/11?apikey={"$APIKEY"})" || die "Unable to connect to API."
+	FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
+	if [[ $STATUS != success ]]; then
+		if [[ -n $MESSAGE ]]; then
+			die "$MESSAGE"
+		else
+			die "An unknown API error has occurred. Please try again later."
+		fi
+	fi
+	FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
 
-echo "[+] Contacting VPN_API in Server-FR_PARIS-."
-echo
-                        RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"11","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/11?apikey={"$APIKEY"})" || die "Unable to connect to DAV_VPN_API."
-    FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
-    if [[ $STATUS != success ]]; then
-        if [[ -n $MESSAGE ]]; then
-            die "$MESSAGE"
-        else
-            die "An unknown API error has occurred. Please try again later."
-        fi
-    fi
-    FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
+	echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/wgFR.conf'."
+	umask 077
+	mkdir -p /etc/wireguard/
+	touch /etc/wireguard/wgFR.conf
+	rm -r /etc/wireguard/wgFR.conf
+	cat > /etc/wireguard/wgFR.conf <<-_EOF
+		[Interface]
+		PrivateKey = $CLIENT_PRIVKEY
+		Address = $CLIENT_IPV4/32
+		DNS = $SERVER_DNS
 
-    echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/...conf'."
-    umask 077
-    mkdir -p /etc/wireguard/
-    #touch /etc/wireguard/wgFR.conf
-    rm -r /etc/wireguard/wgFR.conf
-    cat > /etc/wireguard/wgFR.conf <<-_EOF
-        [Interface]
-        PrivateKey = $CLIENT_PRIVKEY
-        Address = $CLIENT_IPV4/32
-        DNS = $SERVER_DNS
+		[Peer]
+		PublicKey = $SERVER_PUBKEY
+		Endpoint = $SERVER_IP:$SERVER_PORT
+		AllowedIPs = 0.0.0.0/0, ::/0
 
-        [Peer]
-        PublicKey = $SERVER_PUBKEY
-        Endpoint = $SERVER_IP:$SERVER_PORT
-        AllowedIPs = 0.0.0.0/0, ::/0
-_EOF
-        
-        echo "[+++++] Success. The following commands may be run for connecting to VPN:"
+		_EOF
 
-        echo
-    echo "  \$ wg-quick up wgFR"
-#    echo "  \$ wg-quick up wgPL"
+		echo "[+++] Success. The following commands may be run for connecting to VPN:"
+	echo
+	echo "  \$ wg-quick up wgFR"
+	#echo "  \$ wg-quick up wgPL"
 
 echo
 echo "[!!]Please wait up to 60 seconds for your public key to be added to the servers."
-
+#####################################################
 #echo "== Script avec confirmation =="
 
 echo 
@@ -114,56 +114,52 @@ if confirm "Do you want to display one of your configuration?"; then
     #cat  /etc/wireguard/wgPL.conf
 else
     echo "Good!"
-    echo
 fi
-
-    
-
+##################################################################################################################################
+	
                         ;;
                 2)
                         # Appel de la fonction restaure
-                        PL_VPN
-                        echo "[+] Contacting VPN_API in Server-PL_WAW-."
-echo
+                        Server-PL
                         RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"10","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/10?apikey={"$APIKEY"})" || die "Unable to connect to DAV_VPN_API."
-    FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
-    if [[ $STATUS != success ]]; then
-        if [[ -n $MESSAGE ]]; then
-            die "$MESSAGE"
-        else
-            die "An unknown API error has occurred. Please try again later."
-        fi
-    fi
-    FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
+	FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
+	if [[ $STATUS != success ]]; then
+		if [[ -n $MESSAGE ]]; then
+			die "$MESSAGE"
+		else
+			die "An unknown API error has occurred. Please try again later."
+		fi
+	fi
+	FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
 
-    echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/...conf'."
-    umask 077
-    mkdir -p /etc/wireguard/
-    #touch /etc/wireguard/wgPL.conf
-    rm -r /etc/wireguard/wgPL.conf
-    cat > /etc/wireguard/wgPL.conf <<-_EOF
-        [Interface]
-        PrivateKey = $CLIENT_PRIVKEY
-        Address = $CLIENT_IPV4/32
-        DNS = $SERVER_DNS
+	echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/wgPL.conf'."
+	umask 077
+	mkdir -p /etc/wireguard/
+	touch /etc/wireguard/wgPL.conf
+	rm -r /etc/wireguard/wgPL.conf
+	cat > /etc/wireguard/wgPL.conf <<-_EOF
+		[Interface]
+		PrivateKey = $CLIENT_PRIVKEY
+		Address = $CLIENT_IPV4/32
+		DNS = $SERVER_DNS
 
-        [Peer]
-        PublicKey = $SERVER_PUBKEY
-        Endpoint = $SERVER_IP:$SERVER_PORT
-        AllowedIPs = 0.0.0.0/0, ::/0
-_EOF
-        
-        echo "[+++++] Success. The following commands may be run for connecting to VPN:"
+		[Peer]
+		PublicKey = $SERVER_PUBKEY
+		Endpoint = $SERVER_IP:$SERVER_PORT
+		AllowedIPs = 0.0.0.0/0, ::/0
 
-        echo
-    echo "  \$ wg-quick up wgPL"
-#    echo "  \$ wg-quick up wgPL"
+		_EOF
+
+		echo "[+++] Success. The following commands may be run for connecting to VPN:"
+	echo
+	echo "  \$ wg-quick up wgPL"
+	#echo "  \$ wg-quick up wgPL"
 
 echo
 echo "[!!]Please wait up to 60 seconds for your public key to be added to the servers."
-
+#####################################################
 #echo "== Script avec confirmation =="
 
 echo 
@@ -184,90 +180,91 @@ confirm()
 
 if confirm "Do you want to display one of your configuration?"; then
     echo
-    read -p "[?] Which configuration Do you want to display * Answer with the country code 'PL':  " -r PAYS
+    read -p "[?] Which configuration Do you want to display * Answer with the country code 'FR':  " -r PAYS
     echo
     cat  /etc/wireguard/wg"$PAYS".conf
     #cat  /etc/wireguard/wgPL.conf
 else
     echo "Good!"
-    echo
 fi
+##################################################################################################################################
 
                         ;;
                 3)
-                        ALL_VPN
-                        # Appel de la fonction restaure
+                        echo "Server ALL"
                         RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"11","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/11?apikey={"$APIKEY"})" || die "Unable to connect to DAV_VPN_API."
-    FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
-    if [[ $STATUS != success ]]; then
-        if [[ -n $MESSAGE ]]; then
-            die "$MESSAGE"
-        else
-            die "An unknown API error has occurred. Please try again later."
-        fi
-    fi
-    FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
+	FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
+	if [[ $STATUS != success ]]; then
+		if [[ -n $MESSAGE ]]; then
+			die "$MESSAGE"
+		else
+			die "An unknown API error has occurred. Please try again later."
+		fi
+	fi
+	FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
 
-    echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/wgFR.conf'."
-    umask 077
-    mkdir -p /etc/wireguard/
-    #touch /etc/wireguard/wgFR.conf
-    rm -r /etc/wireguard/wgFR.conf
-    cat > /etc/wireguard/wgFR.conf <<-_EOF
-        [Interface]
-        PrivateKey = $CLIENT_PRIVKEY
-        Address = $CLIENT_IPV4/32
-        DNS = $SERVER_DNS
+	echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/wgFR.conf'."
+	umask 077
+	mkdir -p /etc/wireguard/
+	touch /etc/wireguard/wgFR.conf
+	rm -r /etc/wireguard/wgFR.conf
+	cat > /etc/wireguard/wgFR.conf <<-_EOF
+		[Interface]
+		PrivateKey = $CLIENT_PRIVKEY
+		Address = $CLIENT_IPV4/32
+		DNS = $SERVER_DNS
 
-        [Peer]
-        PublicKey = $SERVER_PUBKEY
-        Endpoint = $SERVER_IP:$SERVER_PORT
-        AllowedIPs = 0.0.0.0/0, ::/0
-    _EOF
+		[Peer]
+		PublicKey = $SERVER_PUBKEY
+		Endpoint = $SERVER_IP:$SERVER_PORT
+		AllowedIPs = 0.0.0.0/0, ::/0
 
-echo "[+] Contacting VPN_API in Server-PL_WAW-."
-echo
-                        RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"10","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/10?apikey={"$APIKEY"})" || die "Unable to connect to DAV_VPN_API."
-    FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
-    if [[ $STATUS != success ]]; then
-        if [[ -n $MESSAGE ]]; then
-            die "$MESSAGE"
-        else
-            die "An unknown API error has occurred. Please try again later."
-        fi
-    fi
-    FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
-    IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
+		_EOF
+#####################################################-------------------------------------------------------
+		echo "[+++] Success."
 
-    echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/...conf'."
-    umask 077
-    mkdir -p /etc/wireguard/
-    #touch /etc/wireguard/wgPL.conf
-    rm -r /etc/wireguard/wgPL.conf
-    cat > /etc/wireguard/wgPL.conf <<-_EOF
-        [Interface]
-        PrivateKey = $CLIENT_PRIVKEY
-        Address = $CLIENT_IPV4/32
-        DNS = $SERVER_DNS
+		                RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"10","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/10?apikey={"$APIKEY"})" || die "Unable to connect to DAV_VPN_API."
+	FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
+	if [[ $STATUS != success ]]; then
+		if [[ -n $MESSAGE ]]; then
+			die "$MESSAGE"
+		else
+			die "An unknown API error has occurred. Please try again later."
+		fi
+	fi
+	FIELDS="$(jq -r '.data.server_pubkey,.data.server_dns,.data.client_ipv4,.data.client_privkey,.data.server_port,.data.server_ip' <<<"$RESPONSE")" || die "Unable to parse response."
+	IFS=$'\n' read -r -d '' SERVER_PUBKEY SERVER_DNS CLIENT_IPV4 CLIENT_PRIVKEY SERVER_PORT SERVER_IP <<<"$FIELDS" || true
 
-        [Peer]
-        PublicKey = $SERVER_PUBKEY
-        Endpoint = $SERVER_IP:$SERVER_PORT
-        AllowedIPs = 0.0.0.0/0, ::/0
-_EOF
-echo
-echo "[+++++] Success. The following commands may be run for connecting to VPN:"
+	echo "[++] Writing WriteGuard configuration file to '/etc/wireguard/wgPL.conf'."
+	umask 077
+	mkdir -p /etc/wireguard/
+	touch /etc/wireguard/wgPL.conf
+	rm -r /etc/wireguard/wgPL.conf
+	cat > /etc/wireguard/wgPL.conf <<-_EOF
+		[Interface]
+		PrivateKey = $CLIENT_PRIVKEY
+		Address = $CLIENT_IPV4/32
+		DNS = $SERVER_DNS
 
-        echo
-    echo "  \$ wg-quick up wgPL"
-    echo "  \$ wg-quick up wgFR"
+		[Peer]
+		PublicKey = $SERVER_PUBKEY
+		Endpoint = $SERVER_IP:$SERVER_PORT
+		AllowedIPs = 0.0.0.0/0, ::/0
+
+		_EOF
+
+		echo "[+++] Success."
+		echo "[Info] ▼▼▼▼ The following commands may be run for connecting to VPN ▼▼▼▼:"
+
+	echo "  \$ ► wg-quick up wgFR"
+	echo "  \$ ► wg-quick up wgPL"
 
 echo
 echo "[!!]Please wait up to 60 seconds for your public key to be added to the servers."
-
+#####################################################
 #echo "== Script avec confirmation =="
 
 echo 
@@ -288,18 +285,21 @@ confirm()
 
 if confirm "Do you want to display one of your configuration?"; then
     echo
-    read -p "[?] Which configuration Do you want to display * Answer with the country code 'PL':  " -r PAYS
+    read -p "[?] Which configuration Do you want to display * Answer with the country code 'FR':  " -r PAYS
     echo
     cat  /etc/wireguard/wg"$PAYS".conf
     #cat  /etc/wireguard/wgPL.conf
 else
     echo "Good!"
-    echo
 fi
+
+
+
+
                         ;;
                 4)
                         echo "Bye Bye"
-                        exit 0
+                        Exit 0
                         ;;
                 *)
                         echo "Incorrect choice"
