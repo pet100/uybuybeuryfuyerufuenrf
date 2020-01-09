a2aaa2580abdcca7c0fe99034393822ffbf9cd2d
pet100 <52423716+pet100@users.noreply.github.com>
2020-01-09 16:17:09 +0100

    Update 1233VPN-wg.sh

--- 1233VPN-wg.sh
+++ 1233VPN-wg.sh
@@ -2,16 +2,8 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Copyright (C) 2016-2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.                                                                                     
-                                                   
+                                                           
 
-echo "                  ██╗   ██╗██████╗ ███╗   ██╗ "            
-echo "                  ██║   ██║██╔══██╗████╗  ██║ "            
-echo "      █████╗█████╗██║   ██║██████╔╝██╔██╗ ██║█████╗█████╗ "
-echo "      ╚════╝╚════╝╚██╗ ██╔╝██╔═══╝ ██║╚██╗██║╚════╝╚════╝ "
-echo "                   ╚████╔╝ ██║     ██║ ╚████║ "            
-echo "                    ╚═══╝  ╚═╝     ╚═╝  ╚═══╝ "            
-
-echo
 
 die() {
         echo "[-] Error: Oops, your account number seems to be wron $1" >&2
@@ -27,32 +19,39 @@ SELF="$(cd "${SELF%/*}" && pwd -P)/${SELF##*/}"
 [[ $UID == 0 ]] || exec sudo -p "[?] $PROGRAM must be run as root. Please enter the password for %u to continue: " -- "$BASH" -- "$SELF" "${ARGS[@]}"
 
 [[ ${BASH_VERSINFO[0]} -ge 4 ]] || die "bash ${BASH_VERSINFO[0]} detected, when bash 4+ required"
+echo
+echo $'\e[1;36m                  ██╗   ██╗██████╗ ███╗   ██╗ \e[0m'
+echo $'\e[1;36m                  ██║   ██║██╔══██╗████╗  ██║ \e[0m'            
+echo $'\e[1;36m      █████╗█████╗██║   ██║██████╔╝██╔██╗ ██║█████╗█████╗ \e[0m'
+echo $'\e[1;31m      ╚════╝╚════╝╚██╗ ██╔╝██╔═══╝ ██║╚██╗██║╚════╝╚════╝ \e[0m'
+echo $'\e[1;36m                   ╚████╔╝ ██║     ██║ ╚████║ \e[0m'            
+echo $'\e[1;36m                    ╚═══╝  ╚═╝     ╚═╝  ╚═══╝ \e[0m'    
 
 set -e
 type curl >/dev/null || die "Please install curl and then try again."
 type jq >/dev/null || die "Please install jq and then try again."
-
+echo
 read -p "[?] Please enter your VPN account number: " -r APIKEY
 ##################################################################################################################################
 
     	function Server-FR {
-        echo "Contacting AzireVPN API in Toronto, Canada."
+         echo $'\e[1;34m'[*..]Contacting VPN API in France, Paris.$'\e[0m'
 }
 
 function Server-PL {
-        echo "[*..] Contacting AzireVPN API in Paris, France."
+        echo $'\e[1;34m'[*..]Contacting VPN API in Poland, Olsztyn.$'\e[0m'
 }
 
 function Server-ALL {
-        echo "[*..] Contacting AzireVPN API in Poland, Olsztyn."
+	echo $'\e[1;34m'[*..] Contacting VPN API in ALL Server.$'\e[0m'
 }
 
-PS3="Votre choix : "
+PS3="Your choice : "
 
 select item in "- Server-FR -" "- Server-PL -" "- Server-ALL -" "- Exit -"
 do
     for var in $REPLY; do
-        echo "Vous avez choisi l'item $var : $item"
+        echo "You have chosen option $var : $item"
         case $var in
 ##################################################################################################################################
 
@@ -82,21 +81,19 @@ do
 		PrivateKey = $CLIENT_PRIVKEY
 		Address = $CLIENT_IPV4/32
 		DNS = $SERVER_DNS
-
 		[Peer]
 		PublicKey = $SERVER_PUBKEY
 		Endpoint = $SERVER_IP:$SERVER_PORT
 		AllowedIPs = 0.0.0.0/0, ::/0
-
 		_EOF
 
-		echo "[+++] Success. The following commands may be run for connecting to VPN:"
+		echo $'\e[1;32m'[+++] Success. The following commands may be run for connecting to VPN:$'\e[0m'
 	echo
 	echo "  \$ wg-quick up wgFR"
 	#echo "  \$ wg-quick up wgPL"
 
 echo
-echo "[!!]Please wait up to 60 seconds for your public key to be added to the servers."
+echo $'\e[1;33m'[!!]Please wait up to 60 seconds for your public key to be added to the servers.$'\e[0m'
 #####################################################
 #echo "== Script avec confirmation =="
 
@@ -154,21 +151,19 @@ fi
 		PrivateKey = $CLIENT_PRIVKEY
 		Address = $CLIENT_IPV4/32
 		DNS = $SERVER_DNS
-
 		[Peer]
 		PublicKey = $SERVER_PUBKEY
 		Endpoint = $SERVER_IP:$SERVER_PORT
 		AllowedIPs = 0.0.0.0/0, ::/0
-
 		_EOF
 
-		echo "[+++] Success. The following commands may be run for connecting to VPN:"
+		echo $'\e[1;32m'[+++] Success. The following commands may be run for connecting to VPN:$'\e[0m'
 	echo
 	echo "  \$ wg-quick up wgPL"
 	#echo "  \$ wg-quick up wgPL"
 
 echo
-echo "[!!]Please wait up to 60 seconds for your public key to be added to the servers."
+echo $'\e[1;33m'[!!]Please wait up to 60 seconds for your public key to be added to the servers.$'\e[0m'
 #####################################################
 #echo "== Script avec confirmation =="
 
@@ -201,7 +196,8 @@ fi
 
                         ;;
                 3)
-                        echo "Server ALL"
+                        #echo "Server ALL"
+                        echo $'\e[1;34m'[*..]Contacting VPN API in France, Paris.$'\e[0m'
                         RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"11","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/11?apikey={"$APIKEY"})" || die "Unable to connect to DAV_VPN_API."
 	FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
 	IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
@@ -225,16 +221,14 @@ fi
 		PrivateKey = $CLIENT_PRIVKEY
 		Address = $CLIENT_IPV4/32
 		DNS = $SERVER_DNS
-
 		[Peer]
 		PublicKey = $SERVER_PUBKEY
 		Endpoint = $SERVER_IP:$SERVER_PORT
 		AllowedIPs = 0.0.0.0/0, ::/0
-
 		_EOF
 #####################################################-------------------------------------------------------
-		echo "[+++] Success."
-
+		echo $'\e[1;32m'[+++] Success☼$'\e[0m'
+                        echo $'\e[1;34m'[*..]Contacting VPN API in Poland, Olsztyn.$'\e[0m'
 		                RESPONSE="$(curl -LsS -k --user admin:password --header 'Content-Type: application/json' --header 'Accept: application/json' -X POST --data '{"server_id":"10","client_description":"0000"}'  https://pierro100-eval-test.apigee.net/vpn_fr/api/clients/add_to_server/10?apikey={"$APIKEY"})" || die "Unable to connect to DAV_VPN_API."
 	FIELDS="$(jq -r '.status,.message' <<<"$RESPONSE")" || die "Unable to parse response."
 	IFS=$'\n' read -r -d '' STATUS MESSAGE <<<"$FIELDS" || true
@@ -258,22 +252,20 @@ fi
 		PrivateKey = $CLIENT_PRIVKEY
 		Address = $CLIENT_IPV4/32
 		DNS = $SERVER_DNS
-
 		[Peer]
 		PublicKey = $SERVER_PUBKEY
 		Endpoint = $SERVER_IP:$SERVER_PORT
 		AllowedIPs = 0.0.0.0/0, ::/0
-
 		_EOF
 
-		echo "[+++] Success."
-		echo "[Info] ▼▼▼▼ The following commands may be run for connecting to VPN ▼▼▼▼:"
+		echo $'\e[1;32m'[+++] Success☼$'\e[0m'
+		echo $'\e[1;36m'[Info] ▼▼▼▼ The following commands may be run for connecting to VPN ▼▼▼▼:$'\e[0m'
 
 	echo "  \$ ► wg-quick up wgFR"
 	echo "  \$ ► wg-quick up wgPL"
 
 echo
-echo "[!!]Please wait up to 60 seconds for your public key to be added to the servers."
+echo $'\e[1;33m'[!!]Please wait up to 60 seconds for your public key to be added to the servers.$'\e[0m'
 #####################################################
 #echo "== Script avec confirmation =="
 
@@ -308,13 +300,12 @@ fi
 
                         ;;
                 4)
-                        echo "Bye Bye"
+                        echo $'\e[1;42m'[☻]Bye Bye.............$'\e[0m'
                         Exit 0
                         ;;
                 *)
-                        echo "Incorrect choice"
+                        echo $'\e[1;31m'Incorrect choice$'\e[0m'
                         ;;
         esac
     done
 done
-
